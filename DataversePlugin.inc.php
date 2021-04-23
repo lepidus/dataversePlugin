92d05d48cd302745230ec0d90e7a9140ac26834f
Andrey Souza <andrey@lepidus.com.br>
2021-04-23 16:38:45 -0400

    Creates initial plugin structure.
    Issue: documentacao-e-tarefas/scielo#143
    
    Signed-off-by: Andrey <andrey@lepidus.com.br>
    Signed-off-by: Beatriz <beatrz@lepidus.com.br>

---
+++ DataversePlugin.inc.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file plugins/generic/dataverse/DataversePlugin.inc.php
+ *
+ * Copyright (c) 2013-2019 Simon Fraser University
+ * Copyright (c) 2003-2019 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class DataversePlugin
+ * @ingroup plugins_generic_dataverse
+ *
+ * @brief dataverse plugin class
+ */
+
+import('lib.pkp.classes.plugins.GenericPlugin');
+
+class DataversePlugin extends GenericPlugin {
+
+	/**
+	 * @see LazyLoadPlugin::register()
+	 */
+	public function register($category, $path, $mainContextId = NULL) {
+		$success = parent::register($category, $path, $mainContextId);
+		return $success;
+	}
+
+	/**
+	 * @see PKPPlugin::getDisplayName()
+	 */
+	public function getDisplayName() {
+		return __('plugins.generic.dataverse.displayName');
+	}
+
+	/**
+	 * @see PKPPlugin::getDescription()
+	 */
+	public function getDescription() {
+		return __('plugins.generic.dataverse.description');
+	}
+}
+
+?>
