6413d04affda2b7282654b2273804c5bf74391f0
iudi <iudi@lepidus.com.br>
2021-08-06 17:18:05 -0300

    Retrieve data from submission to SubmissionAdapter.
    Issue: documentacao-e-tarefas/scielo#212
    
    Signed-off-by: Iudi <iudi@lepidus.com.br>
    Signed-off-by: Thiago <thiago@lepidus.com.br>

--- classes/creators/SubmissionAdapterCreator.inc.php
+++ classes/creators/SubmissionAdapterCreator.inc.php
@@ -2,15 +2,36 @@
 
 class SubmissionAdapterCreator
 {
+    private $locale = "en_US";
+    private $subject = "Testing Subject";
+
     public function createSubmissionAdapter($submissionId): SubmissionAdapter
     {
-        $title = '';
-        $authors = array('');
-        $description = '';
-        $keywords = array('');
-        $language = '';
+        $submission = DAORegistry::getDAO('SubmissionDAO')->getById($submissionId);
+        $publication = $submission->getCurrentPublication();
 
-        return new SubmissionAdapter($title, $authors, $description, $keywords, $language);
+        $title = $publication->getLocalizedData('title', $this->locale);
+        $authors = $this->retrieveAuthors($publication);
+        $description = $publication->getLocalizedData('abstract', $this->locale);
+
+        $keywords = [$this->subject];
+
+        return new SubmissionAdapter($title, $authors, $description, $keywords);
     }
 
+    protected function retrieveAuthors($publication)
+    {
+        $authors =  $publication->getData('authors');
+        $authorAdapters = [];
+
+        foreach ($authors as $author) {
+            $fullName = $author->getFullName($this->locale);
+            $affiliation = $author->getLocalizedData('affiliation', $this->locale);
+
+            $affiliation = !is_null($affiliation) ? $affiliation : "";
+            $authorAdapters[] = new AuthorAdapter($fullName, $affiliation);
+        }
+
+        return $authorAdapters;
+    }
 }
