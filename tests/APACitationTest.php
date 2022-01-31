2d3869940ab42b050a7751286f9aabba171ac64d
Hilton dos Santos Costa Neto <hilton@lepidus.com.br>
2022-01-31 15:30:57 -0400

    Define locale pt_BR
    Issue: documentacao-e-tarefas/scielo#286
    
    Signed-off-by: lucasanselmo <lucasanselmo@lepidus.com.br>
    Signed-off-by: hilton <hilton@lepidus.com.br>

--- tests/APACitationTest.php
+++ tests/APACitationTest.php
@@ -33,8 +33,8 @@ class APACitationTest extends PKPTestCase
 			->getMock();
 
 		$journal = new Journal();
-        $journal->setPrimaryLocale('en_US');
-        $journal->setName('Preprints da Lepidus', 'en_US');
+        $journal->setPrimaryLocale('pt_BR');
+        $journal->setName('Preprints da Lepidus', 'pt_BR');
 
 		$journalDAO->expects($this->any())
 		           ->method('getById')
@@ -47,8 +47,8 @@ class APACitationTest extends PKPTestCase
     {
         $author = new Author();
         $author->setData('publicationId', 1234);
-        $author->setGivenName('Iris', 'en_US');
-        $author->setFamilyName('Castanheiras', 'en_US');
+        $author->setGivenName('Iris', 'pt_BR');
+        $author->setFamilyName('Castanheiras', 'pt_BR');
         $this->authors = [$author];
     }
 
@@ -58,7 +58,7 @@ class APACitationTest extends PKPTestCase
         $this->submission->setId(1245);
         $this->submission->setData('contextId', 1);
         $this->submission->setData('dateSubmitted', '2021-05-31 15:38:24');
-        $this->submission->setData('locale', 'en_US');
+        $this->submission->setData('locale', 'pt_BR');
     }
     
     private function createTestPublication(): void
@@ -66,9 +66,9 @@ class APACitationTest extends PKPTestCase
         $this->publication = new Publication();
         $this->publication->setId(1234);
         $this->publication->setData('submissionId', 1245);
-        $this->publication->setData('title', "The Rise of The Machine Empire", 'en_US');
+        $this->publication->setData('title', "The Rise of The Machine Empire", 'pt_BR');
         $this->publication->setData('authors', $this->authors);
-        $this->publication->setData('locale', 'en_US');
+        $this->publication->setData('locale', 'pt_BR');
         $this->publication->setData('relationStatus', '1');
     }
 
@@ -106,8 +106,8 @@ class APACitationTest extends PKPTestCase
 
     public function testFamilyNameWithAccentOnTheFirstLetter(): void
     {
-        $this->authors[0]->setGivenName('Átila', 'en_US');
-        $this->authors[0]->setFamilyName('Álamo', 'en_US');
+        $this->authors[0]->setGivenName('Átila', 'pt_BR');
+        $this->authors[0]->setFamilyName('Álamo', 'pt_BR');
 
         $apaCitation = new APACitation();
         $preprintCitation = $apaCitation->getFormattedCitationBySubmission($this->submission);
@@ -119,8 +119,8 @@ class APACitationTest extends PKPTestCase
 
     public function testGivenNameWithAccentAnyLetterExceptFirstLetterAndFamilyNameWithoutAccent(): void
     {
-        $this->authors[0]->setGivenName('Mário', 'en_US');
-        $this->authors[0]->setFamilyName('Fernandes', 'en_US');
+        $this->authors[0]->setGivenName('Mário', 'pt_BR');
+        $this->authors[0]->setFamilyName('Fernandes', 'pt_BR');
 
         $apaCitation = new APACitation();
         $preprintCitation = $apaCitation->getFormattedCitationBySubmission($this->submission);
@@ -131,8 +131,8 @@ class APACitationTest extends PKPTestCase
 
     public function testFamilyNameWithAccentOnTheFirstLetterAndGivenNameWithoutAccent() : void 
     {
-        $this->authors[0]->setGivenName('Lucas', 'en_US');
-        $this->authors[0]->setFamilyName('Átila', 'en_US');
+        $this->authors[0]->setGivenName('Lucas', 'pt_BR');
+        $this->authors[0]->setFamilyName('Átila', 'pt_BR');
 
         $apaCitation = new APACitation();
         $preprintCitation = $apaCitation->getFormattedCitationBySubmission($this->submission);
@@ -143,8 +143,8 @@ class APACitationTest extends PKPTestCase
 
     public function testGivenNameAndFamilyNameWithAccentExceptFirstLetter(): void
     {
-        $this->authors[0]->setGivenName('Cláudio', 'en_US');
-        $this->authors[0]->setFamilyName('Sérgio', 'en_US');
+        $this->authors[0]->setGivenName('Cláudio', 'pt_BR');
+        $this->authors[0]->setFamilyName('Sérgio', 'pt_BR');
 
         $apaCitation = new APACitation();
         $preprintCitation = $apaCitation->getFormattedCitationBySubmission($this->submission);
@@ -155,7 +155,7 @@ class APACitationTest extends PKPTestCase
 
     public function testGivenNameWithAccentOnTheFirstLetter(): void
     {
-        $this->authors[0]->setGivenName('Ângelo', 'en_US');
+        $this->authors[0]->setGivenName('Ângelo', 'pt_BR');
 
         $apaCitation = new APACitation();
         $preprintCitation = $apaCitation->getFormattedCitationBySubmission($this->submission);
