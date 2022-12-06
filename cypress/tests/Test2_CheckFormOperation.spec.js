589a241672ba37607fc59ece75c43ed3aa490caa
lucasanselmo <lucasanselmo@lepidus.com.br>
2022-12-06 19:06:24 -0300

    Fixes integration test
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

--- cypress/tests/Test2_CheckFormOperation.spec.js
+++ cypress/tests/Test2_CheckFormOperation.spec.js
@@ -1,92 +1,165 @@
+var adminUser = Cypress.env('adminUser');
+var adminPassword = Cypress.env('adminPassword');
+
+function activeDataversePlugin() {
+	cy.login(adminUser, adminPassword);
+	cy.get('a:contains(' + adminUser + '):visible').click();
+	cy.get('a:contains("Dashboard"):visible').click();
+	cy.get('.app__nav a')
+		.contains('Website')
+		.click();
+	cy.get('button[id="plugins-button"]').click();
+	cy.get('body').then($body => {
+		if (
+			!(
+				$body.find(
+					'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > :nth-child(3) > :nth-child(1) > :checked'
+				).length > 0
+			)
+		) {
+			cy.get(
+				'#component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin > :nth-child(3) >'
+			).click();
+			cy.get(
+				'div:contains(\'The plugin "Dataverse Plugin" has been enabled.\')'
+			);
+		}
+	});
+	cy.get(
+		'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras'
+	).click();
+	cy.get(
+		'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)'
+	).click();
+	cy.get('input[name="dataverseUrl"]').invoke(
+		'val',
+		Cypress.env('dataverseURI')
+	);
+	cy.get('input[name="apiToken"]').invoke(
+		'val',
+		Cypress.env('dataverseAPIToken')
+	);
+	cy.get('input[name^="termsOfUse"]').invoke(
+		'val',
+		Cypress.env('dataverseTermsOfUse')
+	);
+	cy.get(
+		'form[id="dataverseAuthForm"] button[name="submitFormButton"]'
+	).click();
+	cy.get("div:contains('Your changes have been saved.')");
+}
+
 describe('Check form operation', function() {
+	it('Configuration Performed', function() {
+		activeDataversePlugin();
+		cy.get(
+			'form[id="dataverseAuthForm"] button[name="submitFormButton"]'
+		).click();
+		cy.get("div:contains('Your changes have been saved.')");
+		cy.logout();
+	});
 
-    var admin = Cypress.env('adminUser');
-	var adminPassword = Cypress.env('adminPassword');
+	it('Dataverse URL not filled', function() {
+		cy.login(adminUser, adminPassword);
+		cy.get('a:contains(' + adminUser + '):visible').click();
+		cy.get('a:contains("Dashboard"):visible').click();
+		cy.get('.app__nav a')
+			.contains('Website')
+			.click();
+		cy.get('button[id="plugins-button"]').click();
+		cy.get(
+			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras'
+		).click();
+		cy.get(
+			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)'
+		).click();
+		cy.get('input[name="dataverseUrl"]').invoke('val', '');
+		cy.get('input[name="apiToken"]').invoke(
+			'val',
+			Cypress.env('dataverseAPIToken')
+		);
+		cy.get(
+			'form[id="dataverseAuthForm"] button[name="submitFormButton"]'
+		).click();
+		cy.contains('This field is required.');
+		cy.logout();
+	});
 
-    it('Configuration Performed', function() {
+	it('Invalid URL', function() {
+		cy.login(adminUser, adminPassword);
+		cy.get('a:contains(' + adminUser + '):visible').click();
+		cy.get('a:contains("Dashboard"):visible').click();
+		cy.get('.app__nav a')
+			.contains('Website')
+			.click();
+		cy.get('button[id="plugins-button"]').click();
+		cy.get(
+			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras'
+		).click();
+		cy.get(
+			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)'
+		).click();
+		cy.get('input[name="dataverseUrl"]').invoke('val', 'invalidURL');
+		cy.get('input[name="apiToken"]').invoke(
+			'val',
+			Cypress.env('dataverseAPIToken')
+		);
+		cy.get(
+			'form[id="dataverseAuthForm"] button[name="submitFormButton"]'
+		).click();
+		cy.contains('Please enter a valid URL.');
+		cy.logout();
+	});
 
-        cy.login(admin, adminPassword);
-        cy.get('a:contains(' + admin + '):visible').click();
-        cy.get('a:contains("Dashboard"):visible').click();
-        cy.get('.app__nav a').contains('Website').click();
-        cy.get('button[id="plugins-button"]').click();
-        cy.get("body").then($body => {
-			if (!($body.find('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > :nth-child(3) > :nth-child(1) > :checked').length > 0)) {
-				cy.get('#component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin > :nth-child(3) >').click();
-				cy.get('div:contains(\'The plugin "Dataverse Plugin" has been enabled.\')');
-			}
-		});
-		cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras').click();
-        cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)').click();
-        cy.get('input[name="dataverseUrl"]').invoke('val', Cypress.env('dataverseURI'));
-        cy.get('input[name="apiToken"]').invoke('val', Cypress.env('dataverseAPIToken'));
-        cy.get('form[id="dataverseAuthForm"] button[name="submitFormButton"]').click();
-        cy.get('div:contains(\'Your changes have been saved.\')');
-        cy.logout();
-    });
+	it('API Token not filled', function() {
+		cy.login(adminUser, adminPassword);
+		cy.get('a:contains(' + adminUser + '):visible').click();
+		cy.get('a:contains("Dashboard"):visible').click();
+		cy.get('.app__nav a')
+			.contains('Website')
+			.click();
+		cy.get('button[id="plugins-button"]').click();
+		cy.get(
+			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras'
+		).click();
+		cy.get(
+			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)'
+		).click();
+		cy.get('input[name="dataverseUrl"]').invoke(
+			'val',
+			Cypress.env('dataverseURI')
+		);
+		cy.get('input[name="apiToken"]').invoke('val', '');
+		cy.get(
+			'form[id="dataverseAuthForm"] button[name="submitFormButton"]'
+		).click();
+		cy.contains('This field is required.');
+		cy.logout();
+	});
 
-    it('Dataverse URL not filled', function() {
-
-        cy.login(admin, adminPassword);
-        cy.get('a:contains(' + admin + '):visible').click();
-        cy.get('a:contains("Dashboard"):visible').click();
-        cy.get('.app__nav a').contains('Website').click();
-        cy.get('button[id="plugins-button"]').click();
-		cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras').click();
-        cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)').click();
-        cy.get('input[name="dataverseUrl"]').invoke('val', '');
-        cy.get('input[name="apiToken"]').invoke('val', Cypress.env('dataverseAPIToken'));
-        cy.get('form[id="dataverseAuthForm"] button[name="submitFormButton"]').click();
-        cy.contains('This field is required.');
-        cy.logout();
-    });
-
-    it('Invalid URL', function() {
-
-        cy.login(admin, adminPassword);
-        cy.get('a:contains(' + admin + '):visible').click();
-        cy.get('a:contains("Dashboard"):visible').click();
-        cy.get('.app__nav a').contains('Website').click();
-        cy.get('button[id="plugins-button"]').click();
-		cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras').click();
-        cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)').click();
-        cy.get('input[name="dataverseUrl"]').invoke('val', 'invalidURL');
-        cy.get('input[name="apiToken"]').invoke('val', Cypress.env('dataverseAPIToken'));
-        cy.get('form[id="dataverseAuthForm"] button[name="submitFormButton"]').click();
-        cy.contains('Please enter a valid URL.');
-        cy.logout();
-    });
-
-    it('API Token not filled', function() {
-
-        cy.login(admin, adminPassword);
-        cy.get('a:contains(' + admin + '):visible').click();
-        cy.get('a:contains("Dashboard"):visible').click();
-        cy.get('.app__nav a').contains('Website').click();
-        cy.get('button[id="plugins-button"]').click();
-		cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras').click();
-        cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)').click();
-        cy.get('input[name="dataverseUrl"]').invoke('val', Cypress.env('dataverseURI'));
-        cy.get('input[name="apiToken"]').invoke('val', '');
-        cy.get('form[id="dataverseAuthForm"] button[name="submitFormButton"]').click();
-        cy.contains('This field is required.');
-        cy.logout();
-    });
-
-    it('Invalid credentials', function() {
-
-        cy.login(admin, adminPassword);
-        cy.get('a:contains(' + admin + '):visible').click();
-        cy.get('a:contains("Dashboard"):visible').click();
-        cy.get('.app__nav a').contains('Website').click();
-        cy.get('button[id="plugins-button"]').click();
-		cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras').click();
-        cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)').click();
-        cy.get('input[name="dataverseUrl"]').invoke('val', Cypress.env('dataverseURI'));
-        cy.get('input[name="apiToken"]').invoke('val', 'invalidToken');
-        cy.get('form[id="dataverseAuthForm"] button[name="submitFormButton"]').click();
-        cy.contains('Can\'t connect to Dataverse');
-        cy.logout();
-    });
-
+	it('Invalid credentials', function() {
+		cy.login(adminUser, adminPassword);
+		cy.get('a:contains(' + adminUser + '):visible').click();
+		cy.get('a:contains("Dashboard"):visible').click();
+		cy.get('.app__nav a')
+			.contains('Website')
+			.click();
+		cy.get('button[id="plugins-button"]').click();
+		cy.get(
+			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras'
+		).click();
+		cy.get(
+			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)'
+		).click();
+		cy.get('input[name="dataverseUrl"]').invoke(
+			'val',
+			Cypress.env('dataverseURI')
+		);
+		cy.get('input[name="apiToken"]').invoke('val', 'invalidToken');
+		cy.get(
+			'form[id="dataverseAuthForm"] button[name="submitFormButton"]'
+		).click();
+		cy.contains("Can't connect to Dataverse");
+		cy.logout();
+	});
 });
--- cypress/tests/Test5_SelectDatasetSubject.spec.js
+++ cypress/tests/Test5_SelectDatasetSubject.spec.js
@@ -4,8 +4,60 @@ const serverPath = Cypress.env('serverPath') || 'publicknowledge';
 
 var submissionId = null;
 
+function activeDataversePlugin() {
+	cy.login(adminUser, adminPassword);
+	cy.get('a:contains(' + adminUser + '):visible').click();
+	cy.get('a:contains("Dashboard"):visible').click();
+	cy.get('.app__nav a')
+		.contains('Website')
+		.click();
+	cy.get('button[id="plugins-button"]').click();
+	cy.get('body').then($body => {
+		if (
+			!(
+				$body.find(
+					'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > :nth-child(3) > :nth-child(1) > :checked'
+				).length > 0
+			)
+		) {
+			cy.get(
+				'#component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin > :nth-child(3) >'
+			).click();
+			cy.get(
+				'div:contains(\'The plugin "Dataverse Plugin" has been enabled.\')'
+			);
+		}
+	});
+	cy.get(
+		'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras'
+	).click();
+	cy.get(
+		'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)'
+	).click();
+	cy.get('input[name="dataverseUrl"]').invoke(
+		'val',
+		Cypress.env('dataverseURI')
+	);
+	cy.get('input[name="apiToken"]').invoke(
+		'val',
+		Cypress.env('dataverseAPIToken')
+	);
+	cy.get('input[name^="termsOfUse"]').invoke(
+		'val',
+		Cypress.env('dataverseTermsOfUse')
+	);
+	cy.get(
+		'form[id="dataverseAuthForm"] button[name="submitFormButton"]'
+	).click();
+	cy.get("div:contains('Your changes have been saved.')");
+}
+
 describe('Defines a subject to submission dataset', function() {
-    it('Dataverse Plugin Configuration', function() {
+	it('Dataverse Plugin Configuration', function() {
+		activeDataversePlugin();
+	});
+
+	it('Check subject field is visible when submission has research data', function() {
 		cy.login(adminUser, adminPassword);
 		cy.get('a')
 			.contains(adminUser)
@@ -13,85 +65,59 @@ describe('Defines a subject to submission dataset', function() {
 		cy.get('a')
 			.contains('Dashboard')
 			.click();
-		cy.get('.app__nav a')
-			.contains('Website')
+		cy.get(
+			'a:contains("Make a New Submission"), div#myQueue a:contains("New Submission")'
+		).click();
+
+		cy.get('input[id^="checklist-"]').click({multiple: true});
+		cy.get('input[id=privacyConsent]').click();
+		cy.get('input[name=userGroupId]')
+			.parent()
+			.contains('Preprint Server manager')
 			.click();
-		cy.get('button[id="plugins-button"]').click();
-		cy.get(
-			'#component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin > :nth-child(3) [type="checkbox"]'
-		).check();
-		cy.wait(2000);
-		cy.get(
-			'#component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin > :nth-child(3) [type="checkbox"]'
-		).should('be.checked');
-		cy.get(
-			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin"] > .first_column > .show_extras'
-		).click();
-		cy.get(
-			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-control-row"] > td > :nth-child(1)'
-		).click();
-		cy.get('input[name="dataverseUrl"]').invoke(
-			'val',
-			Cypress.env('dataverseURI')
-		);
-		cy.get('input[name="apiToken"]').invoke(
-			'val',
-			Cypress.env('dataverseAPIToken')
-		);
-		cy.get(
-			'form[id="dataverseAuthForm"] button[name="submitFormButton"]'
-		).click();
-		cy.get('div:contains("Your changes have been saved.")');
+		cy.get('button.submitFormButton').click();
+
+		cy.get('button:contains("Upload research data")').click();
+		cy.get('[data-modal="datasetModal"]').then($datasetModal => {
+			cy.fixture('dummy.pdf', 'base64').then(fileContent => {
+				cy.get('input[type=file]').upload({
+					fileContent,
+					fileName: 'Data Table.pdf',
+					mimeType: 'application/pdf',
+					encoding: 'base64'
+				});
+			});
+			cy.get('input[name="termsOfUse"').check();
+			cy.get('[data-modal="datasetModal"] button:contains("Save")').click();
+		});
+
+		cy.location('search').then(search => {
+			submissionId = parseInt(search.split('=')[1], 10);
+		});
+
+		cy.waitJQuery();
+		cy.get('button.submitFormButton').click();
+
+		cy.get('select[id^="datasetSubject"').should('be.visible');
 	});
 
-    it('Check subject field is visible when submission has research data', function() {
-        cy.login(adminUser, adminPassword);
-        cy.get('a').contains(adminUser).click();
-		cy.get('a').contains('Dashboard').click();
-        cy.get(
-            'a:contains("Make a New Submission"), div#myQueue a:contains("New Submission")'
-        ).click();
-
-        cy.get('input[id^="checklist-"]').click({multiple: true});
-        cy.get('input[id=privacyConsent]').click();
-        cy.get('input[name=userGroupId]').parent().contains('Preprint Server manager').click();
-        cy.get('button.submitFormButton').click();
-
-        cy.get('button:contains("Upload research data")').click();
-        cy.get('[data-modal="datasetModal"]').then($datasetModal => {
-            cy.fixture('dummy.pdf', 'base64').then(fileContent => {
-                cy.get('input[type=file]').upload({
-                    fileContent,
-                    fileName: 'Data Table.pdf',
-                    mimeType: 'application/pdf',
-                    encoding: 'base64'
-                });
-            });
-            cy.get('input[name="termsOfUse"').check();
-            cy.get('[data-modal="datasetModal"] button:contains("Save")').click();
-        });
-
-        cy.location('search').then(search => {
-            submissionId = parseInt(search.split('=')[1], 10);
-        });
-
-        cy.waitJQuery();
-	    cy.get('button.submitFormButton').click();
-
-        cy.get('select[id^="datasetSubject"').should('be.visible');
-    });
-
-    it('Check subject field is not visible when submission not has research data', function() {
-        cy.login(adminUser, adminPassword);
-        cy.visit(
-			'index.php/' + serverPath + '/submission/wizard/2?submissionId=' + submissionId + '#step-2'
+	it('Check subject field is not visible when submission not has research data', function() {
+		cy.login(adminUser, adminPassword);
+		cy.visit(
+			'index.php/' +
+				serverPath +
+				'/submission/wizard/2?submissionId=' +
+				submissionId +
+				'#step-2'
 		);
-        cy.get('a[name="step-2"]').click();
-        cy.get('.listPanel__item:contains(Data Table.pdf) .listPanel__itemActions button').click();
-        cy.get('[data-modal="delete"] button:contains(Yes)').click();
-        cy.waitJQuery();
-        cy.get('button.submitFormButton').click();
+		cy.get('a[name="step-2"]').click();
+		cy.get(
+			'.listPanel__item:contains(Data Table.pdf) .listPanel__itemActions button'
+		).click();
+		cy.get('[data-modal="delete"] button:contains(Yes)').click();
+		cy.waitJQuery();
+		cy.get('button.submitFormButton').click();
 
-        cy.get('select[id^="datasetSubject"').should('not.be.visible');
-    });
+		cy.get('select[id^="datasetSubject"').should('not.be.visible');
+	});
 });
