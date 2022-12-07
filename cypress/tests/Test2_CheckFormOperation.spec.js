eaf0c491b863f99408cdfa7e46b62d9c91164acb
Thiago Matos <thiago@lepidus.com.br>
2022-12-07 14:46:34 -0400

    Adds integration test to check terms of use url
    Issue: documentacao-e-tarefas/scielo#432
    
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

--- cypress/tests/Test2_CheckFormOperation.spec.js
+++ cypress/tests/Test2_CheckFormOperation.spec.js
@@ -89,6 +89,28 @@ describe('Check form operation', function() {
 		cy.logout();
 	});
 
+	it('Terms of Use not filled', function() {
+		cy.login(adminUser, adminPassword);
+		cy.get('a:contains(' + adminUser + '):visible').click();
+		cy.get('a:contains("Dashboard"):visible').click();
+		cy.get('.app__nav a').contains('Website').click();
+		cy.get('button[id="plugins-button"]').click();
+		cy.get('#component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin').then($pluginRow => {
+			if (!$pluginRow.find('input[id^="select-cell-dataverseplugin-enable"]:checked').length) {
+				cy.get('input[id^="select-cell-dataverseplugin-enable"]').check();
+				cy.get('div').contains('The plugin "Dataverse Plugin" has been enabled');
+			}
+			cy.wrap($pluginRow).contains('Settings').click();
+			cy.get('a[id*="dataverseplugin-settings-button"]').click();
+			cy.get('input[name="dataverseUrl"]').invoke('val', Cypress.env('dataverseURI'));
+			cy.get('input[name="apiToken"]').invoke('val', Cypress.env('dataverseAPIToken'));
+			cy.get('input[name^="termsOfUse"]').first().invoke('val', '');
+			cy.get('form[id="dataverseAuthForm"] button[name="submitFormButton"]').click();
+			cy.contains('This field is required.');
+		});		
+		cy.logout();
+	});
+
 	it('Invalid credentials', function() {
 		cy.login(adminUser, adminPassword);
 		cy.get('a:contains(' + adminUser + '):visible').click();
