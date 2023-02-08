b1dae4797013447d7b3c1d34fa1cb227e2f1ef9d
Thiago Matos <thiago@lepidus.com.br>
2023-02-08 16:09:41 -0400

    Creates test to set up plugin integration tests environment
    Issue: documentacao-e-tarefas/scielo#462
    
    Sign-off-by: Richard <richard@lepidus.com.br>
    Sign-off-by: Thiago <thiago@lepidus.com.br>

--- cypress/tests/Test4_EditDraftDatasetMetadata.spec.js
+++ cypress/tests/Test4_EditDraftDatasetMetadata.spec.js
@@ -28,55 +28,6 @@ let submissionData = {
 	]
 };
 
-let moderator = {
-	'username': 'hermesf',
-	'password': 'hermesfhermesf',
-	'email': 'hermesf@mailinator.com',
-	'givenName': 'Hermes',
-	'familyName': 'Fernandes',
-	'country': 'Brazil',
-	'affiliation': 'Dataverse Project',
-	'roles': ['Moderator']
-};
-
-describe('Tests setup', function() {
-	it('Creates a context', function() {
-		cy.login(adminUser, adminPassword);
-		cy.get('a').contains(adminUser).click();
-		cy.get('a').contains('Dashboard').click();
-		cy.get('.app__nav a').contains('Administration').click();
-		cy.get('a').contains('Hosted Servers').click();
-
-		cy.get('div[id=contextGridContainer]').find('a').contains('Create').click();
-
-		cy.wait(2000);
-		cy.get('input[name="name-en_US"]').type('Dataverse Preprints', {delay: 0});
-		cy.get('input[name=acronym-en_US]').type('DVNP', {delay: 0});
-		cy.get('span').contains('Enable this preprint server').siblings('input').check();
-		cy.get('input[name="supportedLocales"][value="en_US').check();
-		cy.get('input[name="primaryLocale"][value="en_US').check();
-		cy.get('input[name=urlPath]').type('dvnpreprints', {delay: 0});
-
-		cy.get('button').contains('Save').click();
-
-		cy.contains('Settings Wizard', {timeout: 30000});
-	});
-
-	it('Creates a moderator user', function() {
-		cy.login(adminUser, adminPassword);
-		cy.get('a').contains(adminUser).click();
-		cy.get('a:contains("Dashboard")').click();
-		cy.get('a:contains("Users & Roles")').click();
-
-		cy.createUser(moderator);
-		cy.logout();
-
-		cy.login(moderator.username);
-		cy.resetPassword(moderator.username, moderator.password);
-		cy.logout();
-	});
-});
-
 describe('Deposit Draft Dataset', function () {
 	it('Dataverse Plugin Configuration', function () {
 		cy.login(adminUser, adminPassword);
