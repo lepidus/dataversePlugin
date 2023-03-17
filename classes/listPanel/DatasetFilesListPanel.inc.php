86bfcff8ee73afd9ad4913bcd8d85868b4eb662a
Thiago Matos <thiago@lepidus.com.br>
2023-03-17 11:42:19 -0400

    Adds modal configs to DatasetFilesListPanel class
    Issue: documentacao-e-tarefas/scielo#490
    
    Signed-off-by: Thiago <thiago@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

--- classes/listPanel/DatasetFilesListPanel.inc.php
+++ classes/listPanel/DatasetFilesListPanel.inc.php
@@ -4,9 +4,13 @@ use PKP\components\listPanels\ListPanel;
 
 class DatasetFilesListPanel extends ListPanel
 {
+    public $addFileLabel = '';
+
     public $apiUrl = '';
 
     public $isLoading = false;
+
+    public $modalTitle = '';
 
     public function __construct($id, $title, $args = [])
     {
@@ -20,8 +24,10 @@ class DatasetFilesListPanel extends ListPanel
         $config = array_merge(
             $config,
             [
+                'addFileLabel' => $this->addFileLabel,
                 'apiUrl' => $this->apiUrl,
                 'isLoading' => $this->isLoading,
+                'modalTitle' => $this->modalTitle,
             ]
         );
 
