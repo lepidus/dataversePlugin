37c3a0f4d301800378aca7788a60164774c3174c
Thiago Matos <thiago@lepidus.com.br>
2023-03-23 14:17:26 -0400

    Implements class NativeAPICollectionOperations
    Issue: documentacao-e-tarefas/scielo#497
    
    Signed-off-by: Thiago <thiago@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

---
+++ classes/dataverseAPI/operations/implementations/NativeAPICollectionOperations.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+import('plugins.generic.dataverse.classes.dataverseAPI.operations.NativeAPIDataverseOperations');
+import('plugins.generic.dataverse.classes.dataverseAPI.operations.interfaces.CollectionOperationsInterface');
+
+class NativeAPICollectionOperations extends NativeAPIDataverseOperations implements CollectionOperationsInterface
+{
+    public function createDataset(string $datasetPackagePath): DataverseStudy
+    {
+        $apiURL = $this->createAPIURL(['dataverses', $this->collectionAlias, 'datasets']);
+        $requestType = 'POST';
+        $headers = array_merge(
+            $this->getDataverseHeaders(),
+            ['Content-Type' => 'application/json']
+        );
+        $options = [
+            'headers' => $headers,
+            'body' => GuzzleHttp\Psr7\Utils::tryFopen($datasetPackagePath, 'rb')
+        ];
+
+        $response = $this->executeRequest($requestType, $apiURL, $options);
+
+        if ($response->getStatusCode() !== 201) {
+            throw new Exception('Error creating dataset: ' . $response->getMessage());
+        }
+
+        return $this->retrieveDatasetIdentifier($response->getData());
+    }
+
+    public function retrieveDatasetIdentifier(string $responseData): DatasetIdentifier
+    {
+        $datasetData = json_decode($responseData, true)['data'];
+        $datasetIdentifier = new DatasetIdentifier(
+            $datasetData['id'],
+            $datasetData['persistentId']
+        );
+        return $datasetIdentifier;
+    }
+}
