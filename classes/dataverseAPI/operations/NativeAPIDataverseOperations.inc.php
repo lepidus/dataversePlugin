ae79d4c2ca80f18565ec81d8d1f13f9fef143280
Thiago Matos <thiago@lepidus.com.br>
2023-03-23 14:05:03 -0400

    Create abstract class NativeAPIDataverseOperations
    Issue: documentacao-e-tarefas/scielo#497
    
    Signed-off-by: Thiago <thiago@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

---
+++ classes/dataverseAPI/operations/NativeAPIDataverseOperations.inc.php
@@ -0,0 +1,60 @@
+<?php
+
+abstract class NativeAPIDataverseOperations
+{
+    protected $serverURL;
+
+    protected $apiToken;
+
+    protected $collectionAlias;
+
+    public function configure(DataverseCredentials $config): void
+    {
+        $this->serverUrl = $config->getDataverseServerUrl();
+        $this->apiToken = $config->getApiToken();
+        $this->collectionAlias = $config->getDataverseCollection();
+    }
+
+    public function createAPIURL(array $pathParams): string
+    {
+        return $this->getBaseAPIURL() . implode('/', $pathParams);
+    }
+
+    public function getBaseAPIURL(): string
+    {
+        return $this->serverUrl . '/api/';
+    }
+
+    protected function getHttpClient(): \GuzzleHttp\Client
+    {
+        return Application::get()->getHttpClient();
+    }
+
+    protected function getDataverseHeaders(): array
+    {
+        $headers = ['X-Dataverse-key' => $this->apiToken];
+        return $headers;
+    }
+
+    protected function executeRequest(string $type, string $url, array $options): DataverseResponse
+    {
+        try {
+            $response = $this->getHttpClient()->request($type, $url, $options);
+        } catch (GuzzleHttp\Exception\RequestException $e) {
+            $responseMessage = $e->getMessage();
+            if ($e->hasResponse()) {
+                $responseMessage = $e->getResponse()->getBody(true) . ' (' .$e->getResponse()->getStatusCode() . ' ' . $e->getResponse()->getReasonPhrase() . ')';
+            }
+
+            return new DataverseResponse(
+                $e->getCode(),
+                $responseMessage
+            );
+        }
+        return new DataverseResponse(
+            $response->getStatusCode(),
+            $response->getReasonPhrase(),
+            $response->getBody(true)
+        );
+    }
+}
