2c0fcae18eedf88743ea0b13adc080b986718db8
Thiago Matos <thiago@lepidus.com.br>
2023-03-23 16:32:00 -0400

    Fixes guzzle request to send file
    Issue: documentacao-e-tarefas/scielo#497
    
    Signed-off-by: Thiago <thiago@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

--- classes/dataverseAPI/operations/implementations/NativeAPIDatasetOperations.inc.php
+++ classes/dataverseAPI/operations/implementations/NativeAPIDatasetOperations.inc.php
@@ -7,20 +7,23 @@ class NativeAPIDatasetOperations extends NativeAPIDataverseOperations implements
 {
     public function addFile(string $persistentId, DatasetFile $file): DatasetFile
     {
-        $apiURL = $this->createAPIURL(['datasets', ':persistentId', 'add?persistentId=', $persistentId]);
+        $apiURL = $this->createAPIURL(['datasets', ':persistentId', 'add?persistentId=' . $persistentId]);
         $requestType = 'POST';
-        $headers = array_merge(
-            $this->getDataverseHeaders(),
-            ['Content-Type' => 'application/octet-stream']
-        );
+        $headers = $this->getDataverseHeaders();
         $options = [
             'headers' => $headers,
-            'body' => GuzzleHttp\Psr7\Utils::tryFopen($file->getPath(), 'rb')
+            'multipart' => [
+                [
+                    'name'     => 'file',
+                    'contents' => GuzzleHttp\Psr7\Utils::tryFopen($file->getPath(), 'r'),
+                    'filename' => $file->getOriginalFileName()
+                ]
+            ],
         ];
 
         $response = $this->executeRequest($requestType, $apiURL, $options);
 
-        if ($response->getStatusCode() !== HTTP_STATUS_CREATED) {
+        if ($response->getStatusCode() !== HTTP_STATUS_OK) {
             throw new Exception('Error adding file: ' . $response->getMessage());
         }
 
@@ -31,9 +34,9 @@ class NativeAPIDatasetOperations extends NativeAPIDataverseOperations implements
     {
         $fileData = json_decode($responseData, true)['data'];
         $file = new DatasetFile();
-        $file->setId($fileData['dataFile']['id']);
-        $file->setFilename($fileData['label']);
-        $file->originalFileName($fileData['dataFile']['filename']);
+        $file->setId($fileData['files'][0]['dataFile']['id']);
+        $file->setFilename($fileData['files'][0]['label']);
+        $file->setOriginalFileName($fileData['files'][0]['dataFile']['filename']);
 
         return $file;
     }
