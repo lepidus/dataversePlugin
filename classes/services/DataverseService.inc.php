52449299dd9810531b1121b5b17839c692fa1145
Thiago Matos <thiago@lepidus.com.br>
2023-04-12 10:58:50 -0400

    Create class DataverseService
    Issue: documentacao-e-tarefas/scielo#495
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>

---
+++ classes/services/DataverseService.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+import('lib.pkp.classes.log.SubmissionLog');
+import('classes.log.SubmissionEventLogEntry');
+
+abstract class DataverseService
+{
+    protected function registerEventLog(
+        Submission $submission,
+        string $message,
+        array $params = [],
+        int $type = null
+    ): void {
+        $request = Application::get()->getRequest();
+
+        SubmissionLog::logEvent(
+            $request,
+            $submission,
+            $type ?? SUBMISSION_LOG_METADATA_UPDATE,
+            $message,
+            $params
+        );
+    }
+
+    protected function registerAndNotifyError(Submission $submission, string $message, string $error): void
+    {
+        $request = Application::get()->getRequest();
+        $userId = $request->getUser()->getId();
+
+        import('classes.notification.NotificationManager');
+        $notificationMgr = new NotificationManager();
+        $notificationMgr->createTrivialNotification(
+            $userId,
+            NOTIFICATION_TYPE_ERROR,
+            ['contents' => __($message, ['error' => $error])]
+        );
+
+        $this->registerEventLog($submission, $message, ['error' => $error]);
+
+        error_log('Dataverse API error: ' . $message);
+    }
+}
