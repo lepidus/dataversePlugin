efb8cd2076512a101239a274a422036d75b77311
Thiago Matos <thiago@lepidus.com.br>
2023-04-19 15:23:41 -0400

    Add research data state validations and save data in submission
    Issue: documentacao-e-tarefas/scielo#513
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>

--- classes/dispatchers/ResearchDataStateDispatcher.inc.php
+++ classes/dispatchers/ResearchDataStateDispatcher.inc.php
@@ -13,8 +13,9 @@ class ResearchDataStateDispatcher extends DataverseDispatcher
     public function registerHooks(): void
     {
         HookRegistry::register('TemplateManager::display', [$this, 'addResearchDataStateStyles']);
+        HookRegistry::register('submissionsubmitstep1form::Constructor', [$this, 'addResearchDataStateValidate']);
         HookRegistry::register('submissionsubmitstep1form::display', [$this, 'addResearchDataStateField']);
-        HookRegistry::register('Schema::get::submission', [$this, 'addResearchDataStatePropToSubmissionSchema']);
+        HookRegistry::register('Schema::get::submission', [$this, 'addResearchDataStatePropsToSubmissionSchema']);
         HookRegistry::register('SubmissionHandler::saveSubmit', [$this, 'saveResearchDataState']);
         HookRegistry::register('Template::Workflow::Publication', [$this, 'displayResearchDataState']);
     }
@@ -48,6 +49,20 @@ class ResearchDataStateDispatcher extends DataverseDispatcher
         return false;
     }
 
+    public function addResearchDataStateValidate(string $hookName, array $args): bool
+    {
+        $form =& $args[0];
+
+        $form->addCheck(new FormValidatorUrl(
+            $form,
+            'researchDataUrl',
+            'required',
+            'plugins.generic.dataverse.researchDataState.repoAvailable.url.required'
+        ));
+
+        return false;
+    }
+
     public function addResearchDataStateField(string $hookName, array $args): bool
     {
         $request = PKPApplication::get()->getRequest();
@@ -73,5 +88,78 @@ class ResearchDataStateDispatcher extends DataverseDispatcher
             $templateMgr->unregisterFilter('output', array($this, 'researchDataStateFilter'));
         }
         return $output;
+    }
+
+    public function addResearchDataStatePropsToSubmissionSchema(string $hookName, array $args): bool
+    {
+        $schema =& $args[0];
+
+        $schema->properties->{'researchDataState'} = (object) [
+            'type' => 'string',
+            'apiSummary' => true,
+            'validation' => ['nullable'],
+        ];
+
+        $schema->properties->{'researchDataUrl'} = (object) [
+            'type' => 'string',
+            'apiSummary' => true,
+            'validation' => ['nullable'],
+        ];
+
+        $schema->properties->{'researchDataReason'} = (object) [
+            'type' => 'string',
+            'apiSummary' => true,
+            'validation' => ['nullable'],
+        ];
+
+        return false;
+    }
+
+    public function saveResearchDataState(string $hookName, array $args): bool
+    {
+        $step = $args[0];
+        $submissionDao = DAORegistry::getDAO('SubmissionDAO');
+        $stepForm = $args[2];
+
+        if ($step != 1 || !$stepForm->validate()) {
+            return false;
+        }
+
+        $submissionId = $stepForm->execute();
+        $submission = $submissionDao->getById($submissionId);
+
+        $stepForm->readUserVars(['researchDataState']);
+        $researchDataState = $stepForm->getData('researchDataState');
+
+        if (empty($researchDataState)) {
+            $stepForm->addError(
+                'researchDataState',
+                __('plugins.generic.dataverse.researchDataState.required')
+            );
+            return false;
+        }
+
+        $submission->setData('researchDataState', $researchDataState);
+
+        $researchData = $stepForm->getData('researchDataState');
+
+        if ($researchData == RESEARCH_DATA_REPO_AVAILABLE) {
+            $stepForm->readUserVars(['researchDataUrl']);
+            $submission->setData('researchDataUrl', $stepForm->getData('researchDataUrl'));
+        } else {
+            $submission->setData('researchDataUrl', null);
+        }
+
+        if ($researchData == RESEARCH_DATA_PRIVATE) {
+            $stepForm->readUserVars(['researchDataReason']);
+            $submission->setData('researchDataReason', $stepForm->getData('researchDataReason'));
+        } else {
+            $submission->setData('researchDataReason', null);
+        }
+
+        $submissionDao->updateObject($submission);
+        $stepForm->submission = $submission;
+
+        return false;
     }
 }
