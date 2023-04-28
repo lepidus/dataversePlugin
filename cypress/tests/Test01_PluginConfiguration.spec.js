dc8b83fdfb4d2140e0366ff772f1ce24ecd55468
Thiago Matos <thiago@lepidus.com.br>
2023-04-28 12:09:39 -0400

    Refactoring integration tests of plugin configuration
    Issue: documentacao-e-tarefas/scielo#502
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>

---
+++ cypress/tests/Test01_PluginConfiguration.spec.js
@@ -0,0 +1,44 @@
+
+describe('Plugin configuration tests', function () {
+
+    it('Check Configuration', function () {
+        cy.login('dbarnes', null, 'publicknowledge');
+
+		cy.get('a:contains("Website")').click();
+
+		cy.waitJQuery();
+		cy.get('button#plugins-button').click();
+
+		cy.get('input[id^=select-cell-dataverseplugin]').check();
+		cy.get('input[id^=select-cell-dataverseplugin]').should('be.checked');
+
+		cy.get('tr#component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin a.show_extras').click();
+		cy.get('a[id^=component-grid-settings-plugins-settingsplugingrid-category-generic-row-dataverseplugin-settings-button]').click();
+
+		cy.get('input[name=dataverseUrl]').focus().clear();
+		cy.get('input[name=apiToken]').focus().clear();
+		cy.get('input[name="termsOfUse[en_US]"]').focus().clear();
+
+		cy.get('form#dataverseConfigurationForm button:contains("OK")').click();
+		cy.get('label[for^=dataverseUrl].error').should('contain', 'This field is required.');
+		cy.get('label[for^=apiToken].error').should('contain', 'This field is required.');
+		cy.get('label[for^=termsOfUse].error').should('contain', 'This field is required.');
+
+		cy.get('input[name=dataverseUrl]').focus().clear().type('dataverseUrl');
+
+		cy.get('form#dataverseConfigurationForm button:contains("OK")').click();
+		cy.get('label[for^=dataverseUrl].error').should('contain', 'Please enter a valid URL.');
+
+		cy.get('input[name=dataverseUrl]').focus().clear().type(Cypress.env('dataverseUrl'));
+		cy.get('input[name=apiToken]').focus().clear().type('invalidToken');
+		cy.get('input[name="termsOfUse[en_US]"]').focus().clear().type(Cypress.env('termsOfUse'));
+
+		cy.get('form#dataverseConfigurationForm button:contains("OK")').click();
+		cy.contains("Can't connect to Dataverse");
+
+		cy.get('input[name=apiToken]').focus().clear().type(Cypress.env('apiToken'));
+
+		cy.get('form#dataverseConfigurationForm button:contains("OK")').click();
+		cy.get('div:contains("Your changes have been saved.")');
+    });
+});
