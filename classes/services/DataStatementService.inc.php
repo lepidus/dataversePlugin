36d1ee8a722b5c556ddaf42f5b9ea16070f08755
Thiago Matos <thiago@lepidus.com.br>
2023-05-09 11:31:48 -0400

    Renames ResearchDataStateSevice class to DataStatementService
    Issue: documentacao-e-tarefas/scielo#524
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>

---
+++ classes/services/DataStatementService.inc.php
@@ -0,0 +1,63 @@
+<?php
+
+define('DATA_STATEMENT_TYPE_IN_MANUSCRIPT', 0x000000001);
+define('DATA_STATEMENT_TYPE_REPO_AVAILABLE', 0x000000002);
+define('DATA_STATEMENT_TYPE_DATAVERSE_SUBMITTED', 0x000000003);
+define('DATA_STATEMENT_TYPE_ON_DEMAND', 0x000000004);
+define('DATA_STATEMENT_TYPE_PUBLICLY_UNAVAILABLE', 0x000000005);
+
+class DataStatementService
+{
+    public function getResearchDataStates(): array
+    {
+        try {
+            import('plugins.generic.dataverse.dataverseAPI.DataverseClient');
+            $dataverseClient = new DataverseClient();
+            $dataverseCollection = $dataverseClient->getDataverseCollectionActions()->get();
+            $params = [
+                'dataverseName' => $dataverseCollection->getName(),
+            ];
+        } catch (DataverseException $e) {
+            error_log($e->getMessage());
+        }
+
+        return [
+            DATA_STATEMENT_TYPE_IN_MANUSCRIPT => __('plugins.generic.dataverse.researchDataState.inManuscript'),
+            DATA_STATEMENT_TYPE_REPO_AVAILABLE => __('plugins.generic.dataverse.researchDataState.repoAvailable'),
+            DATA_STATEMENT_TYPE_DATAVERSE_SUBMITTED => __(
+                'plugins.generic.dataverse.researchDataState.submissionDeposit',
+                $params
+            ),
+            DATA_STATEMENT_TYPE_ON_DEMAND => __('plugins.generic.dataverse.researchDataState.onDemand'),
+            DATA_STATEMENT_TYPE_PUBLICLY_UNAVAILABLE => __('plugins.generic.dataverse.researchDataState.private')
+        ];
+    }
+
+    public function getResearchDataStateDescription(Publication $publication): string
+    {
+
+        $researchDataState = $publication->getData('researchDataState');
+
+        $statesDescriptions = [
+            DATA_STATEMENT_TYPE_IN_MANUSCRIPT => __(
+                'plugins.generic.dataverse.researchDataState.inManuscript.description'
+            ),
+            DATA_STATEMENT_TYPE_REPO_AVAILABLE => __(
+                'plugins.generic.dataverse.researchDataState.repoAvailable.description',
+                ['researchDataUrl' => $publication->getData('researchDataUrl')]
+            ),
+            DATA_STATEMENT_TYPE_ON_DEMAND => __(
+                'plugins.generic.dataverse.researchDataState.onDemand.description'
+            ),
+            DATA_STATEMENT_TYPE_PUBLICLY_UNAVAILABLE => __(
+                'plugins.generic.dataverse.researchDataState.private.description',
+                ['researchDataReason' => $publication->getData('researchDataReason')]
+            ),
+            DATA_STATEMENT_TYPE_DATAVERSE_SUBMITTED => __('plugins.generic.dataverse.researchData.noResearchData')
+        ];
+
+        $researchDataStateDescription = $statesDescriptions[$researchDataState] ?? __('plugins.generic.dataverse.researchData.noResearchData');
+
+        return $researchDataStateDescription;
+    }
+}
