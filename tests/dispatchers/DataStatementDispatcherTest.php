43def723ce260fa8aaeb52e70033fef9de5c1963
Jhon <jhon@lepidus.com.br>
2024-04-10 16:55:54 -0400

    Adapts test for data statement dispatcher
    Issue: documentacao-e-tarefas/scielo#649
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- tests/dispatchers/DataStatementDispatcherTest.php
+++ tests/dispatchers/DataStatementDispatcherTest.php
@@ -1,8 +1,12 @@
 <?php
 
-import('lib.pkp.tests.DatabaseTestCase');
-import('plugins.generic.dataverse.classes.dispatchers.DataStatementDispatcher');
-import('plugins.generic.dataverse.DataversePlugin');
+use PKP\tests\DatabaseTestCase;
+use PKP\plugins\Hook;
+use APP\submission\Submission;
+use APP\publication\Publication;
+use APP\plugins\generic\dataverse\classes\facades\Repo;
+use APP\plugins\generic\dataverse\classes\dispatchers\DataStatementDispatcher;
+use APP\plugins\generic\dataverse\DataversePlugin;
 
 class DataStatementDispatcherTest extends DatabaseTestCase
 {
@@ -11,26 +15,36 @@ class DataStatementDispatcherTest extends DatabaseTestCase
         return ['publications','publication_settings'];
     }
 
+    private function createTestPublication(array $data): int
+    {
+        $contextId = 1;
+        $context = DAORegistry::getDAO('JournalDAO')->getById($contextId);
+
+        $submission = new Submission();
+        $submission->setData('contextId', $contextId);
+        $publication = new Publication();
+        $publication->setAllData($data);
+
+        $submissionId = Repo::submission()->add($submission, $publication, $context);
+        $submission = Repo::submission()->get($submissionId);
+
+        return $submission->getData('currentPublicationId');
+    }
+
     public function testDataStatementPropsInPublicationSchema(): void
     {
-        $dataStatementTypes = [2, 3, 5];
-        $dataStatementUrls = ['https://example.com', 'https://link.to.data'];
-        $dataStatementReason = 'Has sensitive data';
-
         $plugin = new DataversePlugin();
         $dispatcher = new DataStatementDispatcher($plugin);
+        $publicationData = [
+            'dataStatementTypes' => [2, 3, 5],
+            'dataStatementUrls' => ['https://example.com', 'https://link.to.data'],
+            'dataStatementReason' => 'Has sensitive data'
+        ];
 
-        HookRegistry::register('Schema::get::publication', [$dispatcher, 'addDataStatementToPublicationSchema']);
+        Hook::add('Schema::get::publication', [$dispatcher, 'addDataStatementToPublicationSchema']);
 
-        $publicationDAO = DAORegistry::getDAO('PublicationDAO');
-        $publication = $publicationDAO->newDataObject();
-        $publication->setData('submissionId', rand());
-        $publication->setData('dataStatementTypes', $dataStatementTypes);
-        $publication->setData('dataStatementUrls', $dataStatementUrls);
-        $publication->setData('dataStatementReason', $dataStatementReason);
-
-        $publicationId = $publicationDAO->insertObject($publication);
-        $insertedPublication = $publicationDAO->getById($publicationId);
+        $publicationId = $this->createTestPublication($publicationData);
+        $insertedPublication = Repo::publication()->get($publicationId);
 
         $this->assertEquals($dataStatementTypes, $insertedPublication->getData('dataStatementTypes'));
         $this->assertEquals($dataStatementUrls, $insertedPublication->getData('dataStatementUrls'));
