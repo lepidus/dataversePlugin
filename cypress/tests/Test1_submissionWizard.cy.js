f98b7166e746df5419e44e5d0a94be8a73ee2b00
Jhon <jhon@lepidus.com.br>
2024-04-29 15:25:51 -0400

    Updates integration test for data statement
    Issue: documentacao-e-tarefas/scielo#654
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_submissionWizard.cy.js
+++ cypress/tests/Test1_submissionWizard.cy.js
@@ -1,5 +1,12 @@
 import '../support/commands.js';
 
+function advanceNSteps(n) {
+    for (let stepsAdvanced = 0; stepsAdvanced < n; stepsAdvanced++) {
+        cy.contains('button', 'Continue').click();
+        cy.wait(200);
+    }
+}
+
 describe('Dataverse Plugin - Submission wizard features', function () {
 	let submissionData;
 
@@ -7,7 +14,7 @@ describe('Dataverse Plugin - Submission wizard features', function () {
 		submissionData = {
 			section: 'Articles',
 			title: 'Sustainable Cities: Co-benefits of mass public transportation in climate change mitigation',
-			abstract: 'The transportation industry is the fastest growing energy end-use industry that contributes to greenhouse gases emissions. Mass public transportation can be used as a way to reduce greenhouse gases emissions.',
+			abstract: 'Mass public transportation can be used as a way to reduce greenhouse gases emissions.',
 			keywords: [
                 'mass public transport',
 				'sustainable cities',
@@ -41,19 +48,23 @@ describe('Dataverse Plugin - Submission wizard features', function () {
             cy.get('#titleAbstract-keywords-control-en').type('{enter}', {delay: 0});
         });
 
-        cy.contains('h2', 'Data statement');
-        cy.get('input[id^="dataStatementTypes"][value=2]').click();
-		cy.get('ul[id^="dataStatementUrls"]').should('be.visible');
-		cy.contains('button', 'Continue').click();
+        // cy.contains('h2', 'Data statement');
+        // cy.get('input[id^="dataStatementTypes"][value=2]').click();
+		// cy.get('ul[id^="dataStatementUrls"]').should('be.visible');
+		advanceNSteps(4);
+        cy.get('#reviewDataStatement');
+        cy.contains('h3', 'Data statement');
 		cy.contains('It is required to inform the URLs to the data in repositories');
 
+        cy.get('.pkpSteps__step__label:contains("Details")').click();
         cy.get('input[id^="dataStatementTypes"][value=2]').click();
 		cy.get('ul[id^="dataStatementUrls"]').then((node) => {
 			node.tagit('createTag', 'Example text');
 		});
-		cy.contains('button', 'Continue').click();
-		cy.contains('You must only enter the URLs to the data. Other textual information is not accepted.');
+		advanceNSteps(4);
+		cy.contains('You must only enter the URLs to the data. Other textual information is not accepted');
 
+        cy.get('.pkpSteps__step__label:contains("Details")').click();
         cy.get('input[id^="dataStatementTypes"][value=2]').click();
 		cy.get('ul[id^="dataStatementUrls"]').then((node) => {
 			node.tagit('createTag', 'https://demo.dataverse.org/dataset.xhtml?persistentId=doi:10.5072/FK2/U6AEZM');
@@ -61,10 +72,14 @@ describe('Dataverse Plugin - Submission wizard features', function () {
 
         cy.get('input[id^="dataStatementTypes"][value=5]').click();
 		cy.get('input[id^="dataStatementReason-en"]').should('be.visible');
-		cy.get('button.submitFormButton').click();
-		cy.get('label[for^="dataStatementReason"].error').should('contain', 'This field is required');
+		advanceNSteps(4);
+		cy.contains('It is required to inform the justification for the unavailability of the data');
 
-		cy.get('input[id^="checklist-"]').click({ multiple: true });
-		cy.get('input[id^="dataStatementReason-en"]').focus().clear().type('Has sensitive data');
+		cy.get('.pkpSteps__step__label:contains("Details")').click();
+		cy.get('input[id^="dataStatementReason-en"]').clear().type('Has sensitive data', {delay: 0});
+        advanceNSteps(4);
+        cy.get('#reviewDataStatement').parent().parent().within(() => {
+            cy.get('.pkpNotification--warning').should('not.exist');
+        });
     });
 });
