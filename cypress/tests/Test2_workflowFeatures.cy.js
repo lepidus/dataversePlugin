758e68f50a686c31b1b00e7ecf4d6c6d5ec0060b
Jhon <jhon@lepidus.com.br>
2025-01-27 18:01:09 -0400

    Fixes dataset files badges in integration test
    Issue: documentacao-e-tarefas/scielo#739
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test2_workflowFeatures.cy.js
+++ cypress/tests/Test2_workflowFeatures.cy.js
@@ -127,14 +127,14 @@ describe('Dataverse Plugin - Workflow features', function () {
 		cy.get('form:visible button:contains("Save")').click();
 
         cy.get('#datasetFiles').contains('example.json');
-        cy.get('#datasetTab-button .pkpBadge').contains('2');
+        cy.get('#datasetTab-button .pkpBadge').contains('3');
 
         cy.get('.listPanel__item:contains("example.json") button:contains("Delete")').click();
 		cy.get('.modal__panel--dialog button:contains("Delete File")').click();
         cy.waitJQuery();
 
         cy.get('#datasetFiles').should('not.include.text', 'example.json');
-        cy.get('#datasetTab-button .pkpBadge').contains('1');
+        cy.get('#datasetTab-button .pkpBadge').contains('2');
     });
     it('Author can delete research data in workflow', function () {
         cy.login('eostrom', null, 'publicknowledge');
