d170767fff82d602a44fea1ac855b5abf9617c84
Jhon <jhon@lepidus.com.br>
2026-04-27 17:07:08 -0400

    Adds CrossrefXmlEditor class
    Issue: documentacao-e-tarefas/scielo#902
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ classes/CrossrefXmlEditor.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace APP\plugins\generic\dataverse\classes;
+
+use APP\plugins\generic\dataverse\classes\entities\Dataset;
+use DOMElement;
+
+class CrossrefXmlEditor
+{
+    private const RELATIONS_NAMESPACE = 'http://www.crossref.org/relations.xsd';
+
+    public function addDatasetRelationToWorkNode(DOMElement $workNode, Dataset $dataset): DOMElement
+    {
+        $doc = $workNode->ownerDocument;
+
+        $programNode = $doc->createElementNS(self::RELATIONS_NAMESPACE, 'program');
+
+        $relatedItemNode = $doc->createElementNS(self::RELATIONS_NAMESPACE, 'related_item');
+
+        $descriptionNode = $doc->createElementNS(self::RELATIONS_NAMESPACE, 'description');
+        $descriptionNode->appendChild($doc->createTextNode('Dataset deposited in Dataverse repository.'));
+
+        $doi = preg_replace('/^doi:/i', '', $dataset->getPersistentId());
+
+        $interWorkRelationNode = $doc->createElementNS(self::RELATIONS_NAMESPACE, 'inter_work_relation');
+        $interWorkRelationNode->setAttribute('relationship-type', 'isSupplementedBy');
+        $interWorkRelationNode->setAttribute('identifier-type', 'doi');
+        $interWorkRelationNode->appendChild($doc->createTextNode($doi));
+
+        $relatedItemNode->appendChild($descriptionNode);
+        $relatedItemNode->appendChild($interWorkRelationNode);
+        $programNode->appendChild($relatedItemNode);
+        $workNode->appendChild($programNode);
+
+        return $workNode;
+    }
+}
