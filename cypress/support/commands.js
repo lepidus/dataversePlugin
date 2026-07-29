cd1807217db2242cb36350223fea96aa4bb917ee
Diego Abadan <diego@lepidus.com.br>
2026-07-29 14:37:25 -0300

    Aguarda autocomplete das palavras-chave

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -49,6 +49,10 @@ Cypress.Commands.add('advanceSubmissionSteps', function (numberOfSteps) {
 });
 
 Cypress.Commands.add('addKeyword', function (inputSelector, selectedSelector, keyword) {
-	cy.get(inputSelector).type(keyword, {delay: 0}).type('{enter}', {delay: 0});
+	cy.get(inputSelector)
+		.type(keyword, {delay: 0})
+		.should('have.attr', 'aria-expanded', 'true')
+		.type('{enter}', {delay: 0})
+		.should('have.value', '');
 	cy.get(selectedSelector).contains(keyword);
 });
