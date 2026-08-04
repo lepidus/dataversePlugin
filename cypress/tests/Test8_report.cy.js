25b35a0daa69ad0e1cd61cc0bf71d1ed52399f8f
Jhon <jhon@lepidus.com.br>
2026-08-04 12:07:38 -0400

    Updates cypress test adding new filtering options
    Issue: documentacao-e-tarefas/scielo#934
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test8_report.cy.js
+++ cypress/tests/Test8_report.cy.js
@@ -14,10 +14,23 @@ describe("Dataverse plugin - Report generation", function() {
 
         cy.contains('h1', 'Dataverse Report')
         cy.contains('h2', 'Period');
-        cy.contains('Select the desired submitted date range for filtering');
+        cy.contains('Select the desired filtering type');
+        cy.get('#selectFilterTypeDate').within(() => {
+            cy.contains('option', 'Filter by date submitted');
+            cy.contains('option', 'Filter by final decision');
+        });
+
         cy.contains('legend', 'Submitted date range');
         cy.get('input#startSubmissionDateInterval');
         cy.get('input#endSubmissionDateInterval');
+
+        cy.get('#selectFilterTypeDate').select('Filter by final decision');
+
+        cy.contains('legend', 'Final decision date range');
+        cy.get('input#startFinalDecisionDateInterval');
+        cy.get('input#endFinalDecisionDateInterval');
+
+        cy.get('#selectFilterTypeDate').select('Filter by date submitted');
 
         cy.contains('Generate Report').click();
         cy.wait(2000);
