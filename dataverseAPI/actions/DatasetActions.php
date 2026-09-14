c94dd7c796d70d69487dc8fb361188948eb5aefa
Jhon <jhon@lepidus.com.br>
2026-09-14 18:03:41 -0400

    Adds fixes of minor issues
    Issue: documentacao-e-tarefas/scielo#957
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- dataverseAPI/actions/DatasetActions.php
+++ dataverseAPI/actions/DatasetActions.php
@@ -40,14 +40,10 @@ class DatasetActions extends DataverseActions implements DatasetActionsInterface
             $response = $this->nativeAPIRequest('GET', $uri);
 
             $jsonContent = json_decode($response->getBody(), true);
-            $citation = $jsonContent['datasetVersion']['citation'];
-            $persistentUrl = $jsonContent['persistentUrl'];
-            $citation = str_replace(
-                $persistentUrl,
-                '<a href="' . $persistentUrl . '">' . $persistentUrl . '</a>',
-                $citation
+            $citation = $this->formatCitation(
+                $jsonContent['datasetVersion']['citation'],
+                $jsonContent['persistentUrl']
             );
-            $citation = preg_replace('/,+.UNF[^]]+]/', '', $citation);
         } else {
             $citation = $this->getSWORDCitation($persistentId);
         }
@@ -66,13 +62,20 @@ class DatasetActions extends DataverseActions implements DatasetActionsInterface
         $bibliographicCitation = $doc->getElementsByTagName('bibliographicCitation')->item(0)->nodeValue;
         $persistentUrl = $doc->getElementsByTagName('link')->item(4)->getAttribute('href');
 
-        $citation = str_replace(
-            $persistentUrl,
-            '<a href="' . $persistentUrl . '">' . $persistentUrl . '</a>',
-            $bibliographicCitation
-        );
+        return $this->formatCitation($bibliographicCitation, $persistentUrl);
+    }
 
-        return preg_replace('/,+.UNF[^]]+]/', '', $citation);
+    private function formatCitation(string $citation, string $persistentUrl): string
+    {
+        $citation = preg_replace('/,+.UNF[^]]+]/', '', $citation);
+        $escapedCitation = htmlspecialchars($citation, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+        $escapedPersistentUrl = htmlspecialchars($persistentUrl, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+
+        return str_replace(
+            $escapedPersistentUrl,
+            '<a href="' . $escapedPersistentUrl . '">' . $escapedPersistentUrl . '</a>',
+            $escapedCitation
+        );
     }
 
     public function getDatasetLocks(int $datasetId): array
